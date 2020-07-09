c8bd75d7a13845fcb0adc0f3e885ede0a7c0ee59
Wilfredo Ramirez <rwilfredo534@gmail.com>
2020-07-09 12:05:58 -0400

    final commit

--- main.js
+++ main.js
@@ -21,30 +21,44 @@ let coin = {
     toHTML: function () {
         coin.flip()
         let image = document.createElement('img');
-
+        
         if (this.state === 0) {
-            image.src = "./images/face_up.jpeg"
+            image.src = "./images/faceup.jpg"
             document.body.append(image)
         } else {
-            image.src = "./images/face_down.jpeg"
+            image.src = "./images/facedown.jpg"
             document.body.append(image)
         }
+        return image;
 
     }
 };
 
-//return image;
+
 /* 3. Set the properties of this image element to show either face-up
    or face-down, depending on whether this.state is 0 or 1.*/
 
-//function display20Flips() {
-    
-    //for (let counter = 0; counter <= 20; counter=+1) {
-       // toString () 
-        
-   // }
-        
-    //}
+function display20Flips() {
 
-    /*Write a function called display20Flips that uses a loop to flip the coin 20 times. Each time the coin flips, display the result of each flip as a string on the page (make use of your toString() method)*/
+    for (let counter = 1; counter <= 20; counter += 1) {
+        coin.toString()
 
+    }
+
+}
+display20Flips.call()
+
+/*Write a function called display20Flips that uses a loop to flip the coin 20 times. Each time the coin flips, display the result of each flip as a string on the page (make use of your toString() method)*/
+
+
+function display20images() {
+
+    for (let counter = 1; counter <= 20; counter += 1) {
+        coin.toHTML()
+
+    }
+
+}  
+display20images.call()
+
+/*Write a function called display20Images, again using a loop to flip the coin 20 times…but this time instead of displaying the result as a string, display the result of each flip as an HTML IMG element on the page (make use of your toHTML() method).*/
